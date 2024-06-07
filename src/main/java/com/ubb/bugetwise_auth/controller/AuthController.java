e933adac8052c9671417e10e32e008254b375f3b
andrey100f <andreiforminte19@yahoo.com>
2024-06-07 10:22:42 +0300

    Test endpoint

--- src/main/java/com/ubb/bugetwise_auth/controller/AuthController.java
+++ src/main/java/com/ubb/bugetwise_auth/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.ubb.bugetwise_auth.model.User;
 import com.ubb.bugetwise_auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+
+    @GetMapping
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello from auth service");
+    }
 
     @PostMapping("/register")
     public ResponseEntity<User> register(@RequestBody AuthRequest request) {
